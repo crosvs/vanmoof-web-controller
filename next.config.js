77c8559ee73a411f774d2390bd884a9902ebec1f
Crosvs <github.wielder464@passmail.com>
2026-04-22 04:12:17 +0200

    Git page

--- next.config.js
+++ next.config.js
@@ -1,35 +1,38 @@
 const withPWA = require("next-pwa");
 
 const env = process.env.NODE_ENV;
+const isExport = process.env.NEXT_EXPORT === "true";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: "/api/my_vanmoof_com/:path",
-          destination: "https://my.vanmoof.com/api/v8/:path",
-        },
-        {
-          source: "/api/api_vanmoof-api_com/:path",
-          destination: "https://api.vanmoof-api.com/v8/:path",
-        },
-        {
-          source:
-            "/api/api_vanmoof-api_com/getBikeSharingInvitationsForBike/:path",
-          destination:
-            "https://api.vanmoof-api.com/v8/getBikeSharingInvitationsForBike/:path",
-        },
-        {
-          source: "/api/api_vanmoof-api_com/revokeBikeSharingInvitation/:path",
-          destination:
-            "https://api.vanmoof-api.com/v8/revokeBikeSharingInvitation/:path",
-        },
-      ],
-    };
-  },
+  ...(!isExport && {
+    async rewrites() {
+      return {
+        beforeFiles: [
+          {
+            source: "/api/my_vanmoof_com/:path",
+            destination: "https://my.vanmoof.com/api/v8/:path",
+          },
+          {
+            source: "/api/api_vanmoof-api_com/:path",
+            destination: "https://api.vanmoof-api.com/v8/:path",
+          },
+          {
+            source:
+              "/api/api_vanmoof-api_com/getBikeSharingInvitationsForBike/:path",
+            destination:
+              "https://api.vanmoof-api.com/v8/getBikeSharingInvitationsForBike/:path",
+          },
+          {
+            source: "/api/api_vanmoof-api_com/revokeBikeSharingInvitation/:path",
+            destination:
+              "https://api.vanmoof-api.com/v8/revokeBikeSharingInvitation/:path",
+          },
+        ],
+      };
+    },
+  }),
   pwa: {
     dest: "public",
   },
